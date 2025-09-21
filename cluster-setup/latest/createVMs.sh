de92afa96f0d1708c9e18196ba3b4694a60675a5
Prashanth G L <guledalprashanth@gmail.com>
2025-09-21 22:13:44 +0200

    Update createVMs.sh

--- cluster-setup/latest/createVMs.sh
+++ cluster-setup/latest/createVMs.sh
@@ -1,4 +1,21 @@
-gcloud compute instances create INSTANCE_NAME \
-  --metadata startup-script='#!/bin/bash
-  curl -fsSL https://raw.githubusercontent.com/your/repo/master/script.sh -o /tmp/script.sh
-  sudo bash /tmp/script.sh'
+# Master node 
+gcloud compute instances create cks-master --zone=europe-west4-a \
+--machine-type=e2-medium \
+--image=ubuntu-2404-noble-amd64-v20250920 \
+--image-project=ubuntu-os-cloud \
+--boot-disk-size=50GB \
+--metadata startup-script='#!/bin/bash
+  curl -s https://raw.githubusercontent.com/prashanthgl/cks-course-environment/refs/heads/use-cilium/cluster-setup/latest/install_master.sh -o /tmp/master_script.sh
+  sudo bash /tmp/master_script.sh'
+
+# Worker node
+gcloud compute instances create cks-worker --zone=europe-west4-a \
+--machine-type=e2-medium \
+--image=ubuntu-2404-noble-amd64-v20250920 \
+--image-project=ubuntu-os-cloud \
+--boot-disk-size=50GB \
+--metadata startup-script='#!/bin/bash
+  curl -fsSL https://raw.githubusercontent.com/prashanthgl/cks-course-environment/refs/heads/use-cilium/cluster-setup/latest/install_worker.sh -o /tmp/worker_script.sh
+  sudo bash /tmp/worker_script.sh'
+
+  
