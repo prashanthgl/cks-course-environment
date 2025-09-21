cefc33ef25d3b0c9fecec548d74ab71e487f94e9
Prashanth G L <guledalprashanth@gmail.com>
2025-09-21 17:16:04 +0200

    Create useful_utilities.sh

---
+++ cluster-setup/latest/useful_utilities.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sudo apt install -y git
+git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+curl -L https://github.com/derailed/k9s/releases/latest/download/k9s_Linux_amd64.tar.gz -o k9s.tar.gz
+tar -xzvf k9s.tar.gz
+sudo mv k9s /usr/local/bin/
+rm k9s.tar.gz
+
+sudo apt install -y wget apt-transport-https gnupg lsb-release
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
+echo "deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt update
+sudo apt install -y trivy
+
+curl -fsSL https://falco.org/repo/falcosecurity-packages.asc | sudo apt-key add -
+echo "deb https://download.falco.org/packages/deb stable main" | sudo tee /etc/apt/sources.list.d/falcosecurity.list
+sudo apt update
+sudo apt install -y falco
