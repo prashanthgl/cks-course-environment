14d217fc4cf9e2110027fcb83763557bc9c0852e
Prashanth G L <guledalprashanth@gmail.com>
2025-10-23 15:08:26 +0200

    Update createVMs.sh

--- cluster-setup/createVMs.sh
+++ cluster-setup/createVMs.sh
@@ -5,7 +5,7 @@ gcloud compute instances create cks-master --zone=europe-west4-a \
 --image-project=ubuntu-os-cloud \
 --boot-disk-size=50GB \
 --metadata startup-script='#!/bin/bash
-  curl -s https://raw.githubusercontent.com/prashanthgl/cks-course-environment/refs/heads/use-cilium/cluster-setup/latest/install_master.sh -o /tmp/master_script.sh
+  curl -s https://raw.githubusercontent.com/prashanthgl/cks-course-environment/refs/heads/use-cilium/cluster-setup/latest/install_master.sh -o /tmp/masterscript.sh
   sudo bash /tmp/masterscript.sh'
 
 # Worker node
@@ -15,7 +15,7 @@ gcloud compute instances create cks-worker --zone=europe-west4-a \
 --image-project=ubuntu-os-cloud \
 --boot-disk-size=50GB \
 --metadata startup-script='#!/bin/bash
-  curl -fsSL https://raw.githubusercontent.com/prashanthgl/cks-course-environment/refs/heads/use-cilium/cluster-setup/latest/install_worker.sh -o /tmp/worker_script.sh
+  curl -fsSL https://raw.githubusercontent.com/prashanthgl/cks-course-environment/refs/heads/use-cilium/cluster-setup/latest/install_worker.sh -o /tmp/workerscript.sh
   sudo bash /tmp/workerscript.sh'
 
   
