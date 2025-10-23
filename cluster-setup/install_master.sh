c75da30e7c8f2dffd3ea129b1f9e46dc0e56425d
Prashanth G L <guledalprashanth@gmail.com>
2025-10-23 15:26:03 +0200

    Update install_master.sh

--- cluster-setup/install_master.sh
+++ cluster-setup/install_master.sh
@@ -455,4 +455,4 @@ chown $(id -u):$(id -g) /root/.kube/config
 
 ## Install additional utilities
 echo "Installing additional utilities"
-bash <(curl -s https://raw.githubusercontent.com/prashanthgl/cks-course-environment/refs/heads/use-cilium/cluster-setup/latest/useful_utilities.sh)
+bash <(curl -s https://raw.githubusercontent.com/prashanthgl/cks-course-environment/refs/heads/use-cilium/cluster-setup/useful_utilities.sh)
